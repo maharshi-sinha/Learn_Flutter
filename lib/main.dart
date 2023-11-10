02145ad642cf629cc53930c0af5e2686ecb8b3d7
Maharshi <maharshisinha01@gmail.com>
2023-11-11 03:03:52 +0530

    main.dart page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class FlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Containers',
-      debugShowCheckedModeBanner: false,
+      title: "Flutter App",
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 235, 113, 32)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: Center(
-        child: const MyHomePage(title: 'Flutter Containers Home Page'),
-      ),
+      home: DashboardScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var arrNames = [
-      //taking dynamics data from here i.e. using var we're taking data from arrNames
-      "Maharshi",
-      "Flutter",
-      "Dart",
-      "Android",
-      "iOS",
-      "GitHub",
-      "VSCodes"
-    ];
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Dashboard Screen"),
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Text(
-              //you can also give paddings here
-              arrNames[index],
-              style: TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight
-                      .w500), //instead of any text we're printing arrnames[index]
-            );
-          },
-          itemCount: arrNames.length,
-          //this is the length of the array (.length) for whole length of the array
-          separatorBuilder: (context, index) {
-            //availabe only in ListView.seperated works for seperating the items
-            return Divider(
-              height: 50,
-              color: Colors.black,
-              thickness: 1,
-            );
-          }),
+      body: Container(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Image.asset("assets/images/Dash.png"),
+        ),
+      ),
     );
   }
 }
