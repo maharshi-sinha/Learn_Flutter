2f9ea9b780e38f20079082185854419745d3656c
Maharshi <maharshisinha01@gmail.com>
2023-11-16 03:57:33 +0530

    use of padding

--- lib/main.dart
+++ lib/main.dart
@@ -19,42 +19,17 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard Screen"),
-      ),
-      body: Row(
-        children: [
-          Container(
-            width: 50,
-            height: 100,
-            color: Colors.blue,
-          ),
-          Expanded(
-            //expanded widget fills the remaining space in the screen
-            flex:
-                10, //flex property is used to divide the space in terms of ratio you can say.
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.orange,
+        appBar: AppBar(
+          title: Text("Dashboard Screen"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 11, left: 11),
+          child: Container(
+            child: Text(
+              "Hello World",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
           ),
-          Expanded(
-            flex:
-                2, //here we're dividing the space in 10(orange):2(purple) ratio
-            child: Container(
-              width: 50,
-              height: 100,
-              color: Colors.deepPurple,
-            ),
-          ),
-          Container(
-            width: 50,
-            height: 100,
-            color: Colors.red,
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
