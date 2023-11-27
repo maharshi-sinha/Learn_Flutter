27983376ffc1699031e7daeb42b493fc350d97ee
Maharshi <maharshisinha01@gmail.com>
2023-11-27 18:30:16 +0530

    use of ListTile in ListView Builder

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,79 @@ class FlutterApp extends StatelessWidget {
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var arrNames = [
+      'Apple',
+      'Banana',
+      'Orange',
+      'Mango',
+      'Pineapple',
+      'Grapes',
+      'Watermelon',
+      'Papaya',
+      'Guava',
+      'Kiwi',
+      'Pomegranate',
+      'Strawberry',
+      'Cherry',
+      'Lemon',
+      'Peach',
+      'Pear',
+      'Plum',
+      'Apricot',
+      'Avocado',
+      'Blackberry',
+      'Blueberry',
+      'Coconut',
+      'Cranberry',
+      'Fig',
+      'Grapefruit',
+      'Honeydew',
+      'Kumquat',
+      'Lychee',
+      'Nectarine',
+      'Raspberry',
+      'Tangerine',
+      'Tomato',
+      'Cantaloupe',
+      'Durian',
+      'Elderberry',
+      'Gooseberry',
+      'Jackfruit',
+      'Mangosteen',
+      'Rambutan',
+      'Starfruit',
+      'Tamarind',
+      'Ugli Fruit',
+      'Yuzu',
+      'Zucchini'
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashboard Screen"),
       ),
-      body: Container(
-        color: Colors.blueGrey,
-        margin: EdgeInsets.all(20),
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ),
-      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text("${index + 1}", style: TextStyle(fontSize: 18)),
+              title: Text(arrNames[index]),
+              subtitle: Text("This is the subtitle"),
+              trailing: Icon(Icons.add_circle_sharp),
+              onTap: () {
+                print("Tapped on ${arrNames[index]}");
+              },
+            );
+          },
+          itemCount: arrNames.length,
+          //this is the length of the array (.length) for whole length of the array
+          separatorBuilder: (context, index) {
+            //availabe only in ListView.seperated works for seperating the items
+            return Divider(
+              height: 50,
+              color: Colors.black,
+              thickness: 1,
+            );
+          }),
     );
   }
 }
