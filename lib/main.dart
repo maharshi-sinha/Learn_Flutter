db1a1b3fde0c98953916fb91092a2d17a0ee2f95
Maharshi <maharshisinha01@gmail.com>
2023-11-27 22:52:59 +0530

    circular avatar

--- lib/main.dart
+++ lib/main.dart
@@ -66,31 +66,15 @@ class DashboardScreen extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard Screen"),
-      ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Text("${index + 1}", style: TextStyle(fontSize: 18)),
-              title: Text(arrNames[index]),
-              subtitle: Text("This is the subtitle"),
-              trailing: Icon(Icons.add_circle_sharp),
-              onTap: () {
-                print("Tapped on ${arrNames[index]}");
-              },
-            );
-          },
-          itemCount: arrNames.length,
-          //this is the length of the array (.length) for whole length of the array
-          separatorBuilder: (context, index) {
-            //availabe only in ListView.seperated works for seperating the items
-            return Divider(
-              height: 50,
-              color: Colors.black,
-              thickness: 1,
-            );
-          }),
-    );
+        appBar: AppBar(
+          title: Text("Dashboard Screen"),
+        ),
+        body: Center(
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/Dash.png'),
+            backgroundColor: Colors.yellowAccent,
+            radius: 100,
+          ),
+        ));
   }
 }
