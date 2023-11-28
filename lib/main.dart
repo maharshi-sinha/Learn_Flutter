c9551d9c0e64d2c2a6780c7294ce31fb135d0fbd
Maharshi <maharshisinha01@gmail.com>
2023-11-28 22:39:59 +0530

    adding custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class FlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter App",
-      theme: ThemeData(
-          primarySwatch: Colors.green,
-          scaffoldBackgroundColor: Color.fromARGB(255, 179, 242, 181)),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: DashboardScreen(),
     );
   }
@@ -54,35 +52,15 @@ class DashboardScreen extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard Screen"),
-      ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: AssetImage("assets/images/Dash.png"),
-                backgroundColor: Colors.yellowAccent,
-                radius: 30,
-              ),
-              title: Text(arrNames[index]),
-              subtitle: Text("This is the subtitle"),
-              trailing: Icon(Icons.add_circle_sharp),
-              onTap: () {
-                print("Tapped on ${arrNames[index]}");
-              },
-            );
-          },
-          itemCount: arrNames.length,
-          //this is the length of the array (.length) for whole length of the array
-          separatorBuilder: (context, index) {
-            //availabe only in ListView.seperated works for seperating the items
-            return Divider(
-              height: 50,
-              color: Colors.black,
-              thickness: 1,
-            );
-          }),
-    );
+        appBar: AppBar(
+          title: Text("Dashboard Screen"),
+        ),
+        body: Text(
+          "Hello World",
+          style: TextStyle(
+              fontFamily: 'FontMain',
+              fontSize: 35,
+              fontWeight: FontWeight.w500),
+        ));
   }
 }
