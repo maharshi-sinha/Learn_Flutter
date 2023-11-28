efbaf609f5d89c8d19c1fa04e5d3a8fcb581124f
Maharshi <maharshisinha01@gmail.com>
2023-11-28 17:41:48 +0530

    whatsapp ui

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class FlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter App",
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Color.fromARGB(255, 179, 242, 181)),
       home: DashboardScreen(),
     );
   }
@@ -49,32 +51,38 @@ class DashboardScreen extends StatelessWidget {
       'Lychee',
       'Nectarine',
       'Raspberry',
-      'Tangerine',
-      'Tomato',
-      'Cantaloupe',
-      'Durian',
-      'Elderberry',
-      'Gooseberry',
-      'Jackfruit',
-      'Mangosteen',
-      'Rambutan',
-      'Starfruit',
-      'Tamarind',
-      'Ugli Fruit',
-      'Yuzu',
-      'Zucchini'
     ];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard Screen"),
-        ),
-        body: Center(
-          child: CircleAvatar(
-            backgroundImage: AssetImage('assets/images/Dash.png'),
-            backgroundColor: Colors.yellowAccent,
-            radius: 100,
-          ),
-        ));
+      appBar: AppBar(
+        title: Text("Dashboard Screen"),
+      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/Dash.png"),
+                backgroundColor: Colors.yellowAccent,
+                radius: 30,
+              ),
+              title: Text(arrNames[index]),
+              subtitle: Text("This is the subtitle"),
+              trailing: Icon(Icons.add_circle_sharp),
+              onTap: () {
+                print("Tapped on ${arrNames[index]}");
+              },
+            );
+          },
+          itemCount: arrNames.length,
+          //this is the length of the array (.length) for whole length of the array
+          separatorBuilder: (context, index) {
+            //availabe only in ListView.seperated works for seperating the items
+            return Divider(
+              height: 50,
+              color: Colors.black,
+              thickness: 1,
+            );
+          }),
+    );
   }
 }
