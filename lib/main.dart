6b7c8f1fc1614e5308f4e7536c595d8682e04d69
Maharshi <maharshisinha01@gmail.com>
2023-11-29 01:37:12 +0530

    flutter styles & themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/ui_helper/util.dart';
 
 void main() {
   runApp(FlutterApp());
@@ -9,7 +10,16 @@ class FlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter App",
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+          titleMedium: TextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic),
+        ),
+      ),
       home: DashboardScreen(),
     );
   }
@@ -18,49 +28,66 @@ class FlutterApp extends StatelessWidget {
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var arrNames = [
-      'Apple',
-      'Banana',
-      'Orange',
-      'Mango',
-      'Pineapple',
-      'Grapes',
-      'Watermelon',
-      'Papaya',
-      'Guava',
-      'Kiwi',
-      'Pomegranate',
-      'Strawberry',
-      'Cherry',
-      'Lemon',
-      'Peach',
-      'Pear',
-      'Plum',
-      'Apricot',
-      'Avocado',
-      'Blackberry',
-      'Blueberry',
-      'Coconut',
-      'Cranberry',
-      'Fig',
-      'Grapefruit',
-      'Honeydew',
-      'Kumquat',
-      'Lychee',
-      'Nectarine',
-      'Raspberry',
-    ];
+    // var arrNames = [
+    //   'Apple',
+    //   'Banana',
+    //   'Orange',
+    //   'Mango',
+    //   'Pineapple',
+    //   'Grapes',
+    //   'Watermelon',
+    //   'Papaya',
+    //   'Guava',
+    //   'Kiwi',
+    //   'Pomegranate',
+    //   'Strawberry',
+    //   'Cherry',
+    //   'Lemon',
+    //   'Peach',
+    //   'Pear',
+    //   'Plum',
+    //   'Apricot',
+    //   'Avocado',
+    //   'Blackberry',
+    //   'Blueberry',
+    //   'Coconut',
+    //   'Cranberry',
+    //   'Fig',
+    //   'Grapefruit',
+    //   'Honeydew',
+    //   'Kumquat',
+    //   'Lychee',
+    //   'Nectarine',
+    //   'Raspberry',
+    // ];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard Screen"),
-        ),
-        body: Text(
-          "Hello World",
-          style: TextStyle(
-              fontFamily: 'FontMain',
-              fontSize: 35,
-              fontWeight: FontWeight.w500),
-        ));
+      appBar: AppBar(
+        title: Text("Dashboard Screen"),
+      ),
+      body: Column(
+        children: [
+          Text(
+            "Hello World!",
+            style: Theme.of(context)
+                .textTheme
+                .displayLarge!
+                .copyWith(color: Colors.red),
+          ),
+          Text(
+            "Hello World!",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Text(
+            "Hello World!",
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          Text(
+            "Hello World!",
+            style: mTextstyle11(textColor: Colors.purple),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/ui_helper/util.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+TextStyle mTextstyle11(
+    {Color textColor = Colors.black,
+    FontWeight fontWeight = FontWeight.normal}) {
+  return TextStyle(fontSize: 11, color: textColor, fontWeight: fontWeight);
+}
+
+TextStyle mTextstyle21() {
+  return TextStyle(fontSize: 21);
+}
+
+TextStyle mTextstyle16() {
+  return TextStyle(fontSize: 16);
+}
