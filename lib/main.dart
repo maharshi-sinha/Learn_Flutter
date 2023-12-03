f3bea1f8681432ece56e06c0cb83f93c50609f09
Maharshi <maharshisinha01@gmail.com>
2023-12-04 03:18:27 +0530

    splitting into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,9 @@ void main() {
   runApp(FlutterApp());
 }
 
-class FlutterApp extends StatefulWidget {
-  const FlutterApp({super.key});
+class FlutterApp extends StatelessWidget {
+  const FlutterApp({Key? key}) : super(key: key);
 
-  @override
-  State<FlutterApp> createState() => _FlutterAppState();
-}
-
-class _FlutterAppState extends State<FlutterApp> {
   callBack() {
     print("Clicked!!!");
   }
@@ -28,7 +23,7 @@ class _FlutterAppState extends State<FlutterApp> {
 }
 
 class DashboardScreen extends StatelessWidget {
-  DashboardScreen({super.key});
+  DashboardScreen({Key? key}) : super(key: key);
 
   // var arrColors = [
   //   Colors.red,
@@ -44,12 +39,116 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard"),
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            catItem(), //splitted into widget by making a class below
+            Widget2(),
+            widget3(),
+            widget4(),
+          ],
         ),
-        body: ElevatedButton(
-          child: Text("Click Me"),
-          onPressed: callBack,
-        ));
+      ),
+    );
+  }
+}
+
+class catItem extends StatelessWidget {
+  const catItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        height: 200,
+        color: Colors.red,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(11.0),
+            child: SizedBox(
+              width: 100,
+              child: CircleAvatar(
+                  backgroundColor: Colors.yellowAccent,
+                  backgroundImage: AssetImage('assets/images/Dash.png')),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Widget2 extends StatelessWidget {
+  const Widget2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        color: Colors.blue,
+        child: ListView.builder(
+          itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                ),
+                title: Text("Name"),
+                subtitle: Text("Mobile No"),
+                trailing: Icon(Icons.call),
+              )),
+          itemCount: 10,
+        ),
+      ),
+    );
+  }
+}
+
+class widget3 extends StatelessWidget {
+  const widget3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+          height: 200,
+          color: Colors.yellowAccent,
+          child: ListView.builder(
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(11.0),
+              child: Container(
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(11),
+                    color: Colors.orange),
+              ),
+            ),
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+          )),
+    );
+  }
+}
+
+class widget4 extends StatelessWidget {
+  const widget4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 200,
+        color: Colors.purple,
+      ),
+    );
   }
 }
