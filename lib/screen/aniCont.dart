42b50a22de9b2885a51a1d3fc2fe8a929d61bf92
Maharshi <maharshisinha01@gmail.com>
2023-12-17 02:32:06 +0530

    animated opacity

--- lib/screen/aniCont.dart
+++ lib/screen/aniCont.dart
@@ -6,55 +6,45 @@ class aniCont extends StatefulWidget {
 }
 
 class aniContState extends State<aniCont> {
-  var _width = 200.0;
-  var _height = 100.0;
+  var myOpactity = 1.0;
 
-  Decoration MyDecor = BoxDecoration(
-    borderRadius: BorderRadius.circular(2.0),
-    color: Colors.blueGrey,
-  );
-
-  bool flag = true;
+  bool isVisible = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedContainer(
-                width: _width,
-                height: _height,
-                decoration: MyDecor,
-                curve: Curves.slowMiddle,
-                duration: Duration(seconds: 2)),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (flag) {
-                      _width = 100.0;
-                      _height = 200.0;
-                      MyDecor = BoxDecoration(
-                        borderRadius: BorderRadius.circular(21.0),
-                        color: Colors.orange,
-                      );
-                      flag = false;
-                    } else {
-                      _width = 200.0;
-                      _height = 100.0;
-                      MyDecor = BoxDecoration(
-                        borderRadius: BorderRadius.circular(2.0),
-                        color: Colors.blueGrey,
-                      );
-                      flag = true;
-                    }
-                  });
-                },
-                child: Text("Animate")),
-          ],
+        appBar: AppBar(
+          title: Text("Animated Opacity"),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
+                opacity: myOpactity,
+                duration: Duration(seconds: 1),
+                curve: Curves.elasticInOut,
+                child: Container(
+                  width: 200,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (isVisible) {
+                        myOpactity = 0.0;
+                        isVisible = false;
+                      } else {
+                        myOpactity = 1.0;
+                        isVisible = true;
+                      }
+                    });
+                  },
+                  child: Text("Animate"))
+            ],
+          ),
+        ));
   }
 }
