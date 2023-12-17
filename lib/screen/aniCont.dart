82bd8f2e5de16dcefe3e566f4b1bde2db1d4e511
Maharshi <maharshisinha01@gmail.com>
2023-12-17 14:47:07 +0530

    crossfade animation using initState

--- lib/screen/aniCont.dart
+++ lib/screen/aniCont.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class aniCont extends StatefulWidget {
@@ -6,45 +8,45 @@ class aniCont extends StatefulWidget {
 }
 
 class aniContState extends State<aniCont> {
-  var myOpactity = 1.0;
-
-  bool isVisible = true;
+  bool isFirst = true;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 4), () {
+      isFirst = false;
+      reload();
+    });
+  }
+
+  void reload() {
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Animated Opacity"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedOpacity(
-                opacity: myOpactity,
-                duration: Duration(seconds: 1),
-                curve: Curves.elasticInOut,
-                child: Container(
-                  width: 200,
-                  height: 100,
-                  color: Colors.blue,
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (isVisible) {
-                        myOpactity = 0.0;
-                        isVisible = false;
-                      } else {
-                        myOpactity = 1.0;
-                        isVisible = true;
-                      }
-                    });
-                  },
-                  child: Text("Animate"))
-            ],
+      appBar: AppBar(
+        title: Text("Animated Opacity"),
+      ),
+      body: Center(
+        child: AnimatedCrossFade(
+          duration: Duration(seconds: 2),
+          firstChild: Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
           ),
-        ));
+          secondChild: Image.asset(
+            "assets/images/Dash.png",
+            width: 200,
+            height: 200,
+          ),
+          crossFadeState:
+              isFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        ),
+      ),
+    );
   }
 }
