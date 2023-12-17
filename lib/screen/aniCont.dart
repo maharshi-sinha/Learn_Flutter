e01b1deca2b9d3e0209b34440fb0e4ae391a51cb
Maharshi <maharshisinha01@gmail.com>
2023-12-17 15:10:10 +0530

    cross fade using button

--- lib/screen/aniCont.dart
+++ lib/screen/aniCont.dart
@@ -15,14 +15,19 @@ class aniContState extends State<aniCont> {
     // TODO: implement initState
     super.initState();
 
-    Timer(Duration(seconds: 4), () {
-      isFirst = false;
-      reload();
-    });
+    // Timer(Duration(seconds: 4), () {
+    //   reload();
+    // });
   }
 
   void reload() {
-    setState(() {});
+    setState(() {
+      if (isFirst) {
+        isFirst = false;
+      } else {
+        isFirst = true;
+      }
+    });
   }
 
   Widget build(BuildContext context) {
@@ -31,20 +36,31 @@ class aniContState extends State<aniCont> {
         title: Text("Animated Opacity"),
       ),
       body: Center(
-        child: AnimatedCrossFade(
-          duration: Duration(seconds: 2),
-          firstChild: Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
-          ),
-          secondChild: Image.asset(
-            "assets/images/Dash.png",
-            width: 200,
-            height: 200,
-          ),
-          crossFadeState:
-              isFirst ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedCrossFade(
+              duration: Duration(seconds: 2),
+              firstChild: Container(
+                width: 200,
+                height: 200,
+                color: Colors.red,
+              ),
+              secondChild: Image.asset(
+                "assets/images/Dash.png",
+                width: 200,
+                height: 200,
+              ),
+              crossFadeState: isFirst
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  reload();
+                },
+                child: Text("animate"))
+          ],
         ),
       ),
     );
